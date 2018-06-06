598b9583f354e32cb56d01b076240479231093a1
daniella <daniellabirch@gmail.com>
2018-06-05 18:57:16 -0600

    final commit
    Ideally, I wanted to create arrays that held the elevator and floor objects. My code more or less worked when I was manually creating the floor and elevator objects. Ideally I would create the objects through the classes and manipulate the instances in the arrays.

--- js.js
+++ js.js
@@ -1,4 +1,4 @@
-class elevator {
+class elevatorClass {
 	constructor(requested, currentfloor, doorsopen, occupied, trips) {
 		this.requested = false;
 		this.currentfloor = 0;
@@ -8,7 +8,7 @@ class elevator {
 	}
 }
 
-class floor {
+class floorClass {
 	constructor(requested, elevatorpresent) {
 		this.requested = false;
 		this.elevatorpresent = false;
@@ -22,7 +22,7 @@ var elevators = [
 		currentfloor:0,
 		doorsopen:false,
 		occupied:false,
-		trips:0,
+		trips:0
 	}
 ]
 
@@ -37,6 +37,7 @@ var floors = [
 ]
 
 function requestElevator(requestedfloor){
+//manipulate arrays, this works when I am manually creating the objects
 	var closestElevator = 0;
 
 	for(i = 0; i < elevators.length; i++){
@@ -65,9 +66,16 @@ function requestElevator(requestedfloor){
 	requestedElevator.occupied = true;
 	requestedElevator.trips += 1;
 
-	var floor = new floor(true, true);
+
+//create classes, ideally, I could manipulate the objects in the arrays after they've been created using the classes but I am running out of time, I think I have rethink the logic a bit
+	var floor = new floorClass(true, true);
 	floors.push(floor);
 
-	var elevator = new elevator(true, requestElevator, true, true, 1);
+	var elevator = new elevatorClass(true, requestElevator, true, true, 1);
 	elevators.push(elevator);
 }
+
+document.getElementById("requestelevator").addEventListener("click", function(){
+	//the numbers entered here should be customizable - not sure if the preference should be set by the user or the function itself
+	floors[0].request(0);
+});
